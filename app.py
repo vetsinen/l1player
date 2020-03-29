4e16eebc571e761c60b677a8e8a873950830c4ed
vetsinen <vetsinen@gmail.com>
2020-03-29 14:29:04 +0300

    sqlite db added

--- app.py
+++ app.py
@@ -2,8 +2,24 @@ from flask import Flask
 from flask import render_template
 from os import listdir
 import pathlib
-print(pathlib.Path(__file__).parent.absolute())
+import sqlite3
+from flask import g
+
 app = Flask(__name__)
+
+DATABASE = str(pathlib.Path(__file__).parent.absolute())+'/opinion'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 def process_names(files):
     rez=[]
@@ -22,6 +38,10 @@ def index():
     print(staticpath)
     files = [f for f in listdir(staticpath)]
     return render_template('index.html', files = process_names(files))
+
+@app.route('/opinion', methods=['POST'])
+def opinion():
+    pass
 
 
 if __name__ == '__main__':
